f0d0b3994981a67bb16007d9ec0357356e1050ae
hoorallee <hoorallee@163.com>
2018-02-04 21:00:15 +0800

    第一次提交

---
+++ src/component/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <!-- 根组件中的内容分两种,一种是子页面,一种是子组件 -->
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// 第一步:新建一个webpack的配置文件webpack.config.js
+// 第二步:完成webpack.config.js文件的配置
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    // 入口文件配置
+    entry: "./src/main.js",
+    // 出口文件配置
+    output: {
+        path: path.join(__dirname, 'dist'),
+        // 静态资源在服务器上运行时的访问路径，可以直接http://localhost:8080/dist/bundle.js访问到服务器中的bundle.js文件
+        // publicPath: '/dist',
+        filename: "bundle.js"
+    },
+    // loader加载器
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.less$/, use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'less-loader' }] },
+            { test: /\.scss$/, use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }] },
+            { test: /\.(png|jpg|gif)/, use: [{ loader: 'url-loader', options: { limit: 50000 } }] },
+            { test: /\.vue$/, loader: 'vue-loader' }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: './src/index.html',
+            template: 'template.html',
+            title: '我是标题'
+        })
+    ]
+}
+// 第三步:运行命令webpack
